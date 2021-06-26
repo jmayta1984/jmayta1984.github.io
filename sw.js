518f738845d8d2f821578ac0500a00e13c08aea0
jmayta1984 <38768001+jmayta1984@users.noreply.github.com>
2021-06-26 15:57:33 -0500

    locally cache

--- sw.js
+++ sw.js
@@ -7,6 +7,9 @@ self.addEventListener("install", e => {
 });
 
 self.addEventListener("fetch", e => {
-	console.log(`Interceptin fetch request for: ${e.request.url}`);
-
+	e.respondWith(
+		caches.math(e.request).then(response => {
+			return response || fetch(e.request);
+		})
+	);
 })
